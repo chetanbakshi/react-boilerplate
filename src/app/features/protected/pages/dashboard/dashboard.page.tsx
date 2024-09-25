efe7b19abb0c6dcbe68a1ffb3fc5fe63cee3265c
chetanbakshi <chetan.bakshi@gmail.com>
2024-09-25 15:40:52 +0530

    added welcome message in dashboard page

--- src/app/features/protected/pages/dashboard/dashboard.page.tsx
+++ src/app/features/protected/pages/dashboard/dashboard.page.tsx
@@ -1,7 +1,10 @@
+import { useTypedSelector } from "src/app/core/hooks/use-typed-selectors"
+import './dashboard.style.scss';
 export const DashboardPage = () => {
+    const userInfo = useTypedSelector((state) => state.auth.userInfo);
     return (
-        <div>
-            <h1>Dashboard Page</h1>
+        <div className="dashboard-page-wrapper">
+            <p>Hello <i>{userInfo ? `${userInfo.firstName} ${userInfo.lastName}` : ""}</i>. Welcome to the Dashboard</p> 
         </div>
     )
 }
